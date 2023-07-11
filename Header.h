0c020280e742ea7a2c776c35814c091f0cf7f4b4
arichorn <78001398+arichorn@users.noreply.github.com>
2023-07-11 11:58:59 -0500

    Update _ASDisplayView Headers
    Updated the headers in addition for the updated LowContrastMode

--- Header.h
+++ Header.h
@@ -177,6 +177,8 @@
 @end
 
 @interface _ASDisplayView : UIView
+- (UILabel *)findLabelInSubviews:(NSArray *)subviews;
+- (void)customizeLabel:(UILabel *)label;
 @end
 
 @interface YTCommentDetailHeaderCell : UIView
