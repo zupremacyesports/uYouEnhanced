ae99fce0ba1990a5032739090f25297b7aba9095
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-04-03 17:08:36 -0500

    Create AppIconOptionsController.m

---
+++ Sources/AppIconOptionsController.m
@@ -0,0 +1,64 @@
+#import "AppIconOptionsController.h"
+
+@interface AppIconOptionsController <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (strong, nonatomic) UICollectionView *collectionView;
+@property (strong, nonatomic) UIImageView *iconPreview;
+
+@property (strong, nonatomic) NSArray<NSString *> *appIcons;
+
+@end
+
+@implementation AppIconOptionsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    [self.view addSubview:self.collectionView];
+
+    self.iconPreview = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, 60, 60)];
+    [self.view addSubview:self.iconPreview];
+
+    self.appIcons = @[@"AppIcon1", @"AppIcon2", @"AppIcon3"];
+
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.appIcons.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    UIImage *appIconImage = [UIImage imageNamed:self.appIcons[indexPath.row]];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:appIconImage];
+    imageView.frame = cell.contentView.bounds;
+    [cell.contentView addSubview:imageView];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    UIImage *selectedIconImage = [UIImage imageNamed:self.appIcons[indexPath.row]];
+    self.iconPreview.image = selectedIconImage;
+}
+
+@end
+
+- (void)showAppIconOptions {
+    AppIconOptionsController *appIconOptionsController = [[AppIconOptionsController alloc] init];
+    UINavigationController *appIconOptionsNavController = [[UINavigationController alloc] initWithRootViewController:appIconOptionsController];
+    [self presentViewController:appIconOptionsNavController animated:YES completion:nil];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIBarButtonItem *appIconButton = [[UIBarButtonItem alloc] initWithTitle:@"App Icon" style:UIBarButtonItemStylePlain target:self action:@selector(showAppIconOptions)];
+    self.navigationItem.rightBarButtonItem = appIconButton;
+}
