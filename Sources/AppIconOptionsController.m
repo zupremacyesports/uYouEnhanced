432e0786817b88a1a85f887618d514236dc37f95
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-04-04 00:10:58 -0500

    Add a save button action

--- Sources/AppIconOptionsController.m
+++ Sources/AppIconOptionsController.m
@@ -20,22 +20,28 @@
     self.collectionView.delegate = self;
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
     [self.view addSubview:self.collectionView];
-    
+
     UIButton *defaultButton = [UIButton buttonWithType:UIButtonTypeSystem];
     defaultButton.frame = CGRectMake(20, 100, 100, 40);
     [defaultButton setTitle:@"Default" forState:UIControlStateNormal];
     [defaultButton addTarget:self action:@selector(setDefaultIcon) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:defaultButton];
-    
+
+    UIButton *saveButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    saveButton.frame = CGRectMake(150, 100, 100, 40);
+    [saveButton setTitle:@"Save" forState:UIControlStateNormal];
+    [saveButton addTarget:self action:@selector(saveIcon) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:saveButton];
+
     self.iconPreview = [[UIImageView alloc] initWithFrame:CGRectMake(20, 150, 60, 60)];
     self.iconPreview.layer.cornerRadius = 10.0;
     self.iconPreview.clipsToBounds = YES;
     [self.view addSubview:self.iconPreview];
-        
+
     NSString *path = [[NSBundle mainBundle] pathForResource:@"uYouPlus" ofType:@"bundle"];
     NSBundle *bundle = [NSBundle bundleWithPath:path];
     self.appIcons = [bundle pathsForResourcesOfType:@"png" inDirectory:@"AppIcons"];
-    
+
     if ([UIApplication sharedApplication].supportsAlternateIcons) {
     } else {
         NSLog(@"Alternate icons are not supported on this device.");
@@ -48,17 +54,17 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-    
+
     UIImage *appIconImage = [UIImage imageWithContentsOfFile:self.appIcons[indexPath.row]];
     UIImage *resizedIconImage = [self resizedImageWithImage:appIconImage];
-    
+
     UIImageView *imageView = [[UIImageView alloc] initWithImage:resizedIconImage];
     imageView.contentMode = UIViewContentModeScaleAspectFit;
     imageView.frame = cell.contentView.bounds;
     imageView.layer.cornerRadius = 10.0;
     imageView.clipsToBounds = YES;
     [cell.contentView addSubview:imageView];
-    
+
     return cell;
 }
 
@@ -67,21 +73,12 @@
     UIImage *selectedIconImage = [UIImage imageWithContentsOfFile:self.selectedIconFile];
     UIImage *resizedSelectedIconImage = [self resizedImageWithImage:selectedIconImage];
     self.iconPreview.image = resizedSelectedIconImage;
-    
-    // Set the selected icon as the app's icon
-    [[UIApplication sharedApplication] setAlternateIconName:[self.selectedIconFile lastPathComponent] completionHandler:^(NSError * _Nullable error){
-        if (error) {
-            NSLog(@"Error setting alternate icon: %@", error.localizedDescription);
-        } else {
-            NSLog(@"Alternate icon set successfully");
-        }
-    }];
 }
 
 - (void)setDefaultIcon {
     self.iconPreview.image = nil;
     self.selectedIconFile = nil;
-    
+
     [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error){
         if (error) {
             NSLog(@"Error setting default icon: %@", error.localizedDescription);
@@ -89,6 +86,22 @@
             NSLog(@"Default icon set successfully");
         }
     }];
+}
+
+- (void)saveIcon {
+    if (self.selectedIconFile) {
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        [defaults setObject:self.selectedIconFile forKey:@"SelectedIconFile"];
+        [defaults synchronize];
+
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Success" message:@"App icon saved successfully" preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alertController animated:YES completion:nil];
+    } else {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:@"Please select an app icon before saving" preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
 }
 
 - (UIImage *)resizedImageWithImage:(UIImage *)image {
