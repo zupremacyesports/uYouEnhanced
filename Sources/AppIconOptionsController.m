2873552e3911f2157baae717b54d139caeecde15
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-04-03 23:13:20 -0500

    Even more adjustments.

--- Sources/AppIconOptionsController.m
+++ Sources/AppIconOptionsController.m
@@ -13,28 +13,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
-    self.collectionView.dataSource = self;
-    self.collectionView.delegate = self;
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
-    [self.view addSubview:self.collectionView];
     
-    UIButton *defaultButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    defaultButton.frame = CGRectMake(20, 100, 100, 40);
-    [defaultButton setTitle:@"Default" forState:UIControlStateNormal];
-    [defaultButton addTarget:self action:@selector(setDefaultIcon) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:defaultButton];
-
-    self.iconPreview = [[UIImageView alloc] initWithFrame:CGRectMake(20, 150, 60, 60)];
-    self.iconPreview.layer.cornerRadius = self.iconPreview.frame.size.width / 2;
-    self.iconPreview.clipsToBounds = YES;
-    [self.view addSubview:self.iconPreview];
-
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"uYouPlus" ofType:@"bundle"];
-    NSBundle *bundle = [NSBundle bundleWithPath:path];
-    self.appIcons = [bundle pathsForResourcesOfType:@"png" inDirectory:@"AppIcons"];
+    if ([[UIApplication sharedApplication] supportsAlternateIcons]) {
+        UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+        self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+        self.collectionView.dataSource = self;
+        self.collectionView.delegate = self;
+        [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
+        [self.view addSubview:self.collectionView];
+        
+        UIButton *defaultButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        defaultButton.frame = CGRectMake(20, 100, 100, 40);
+        [defaultButton setTitle:@"Default" forState:UIControlStateNormal];
+        [defaultButton addTarget:self action:@selector(setDefaultIcon) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:defaultButton];
+        
+        self.iconPreview = [[UIImageView alloc] initWithFrame:CGRectMake(20, 150, 60, 60)];
+        self.iconPreview.layer.cornerRadius = 10.0;
+        self.iconPreview.clipsToBounds = YES;
+        [self.view addSubview:self.iconPreview];
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"uYouPlus" ofType:@"bundle"];
+        NSBundle *bundle = [NSBundle bundleWithPath:path];
+        self.appIcons = [bundle pathsForResourcesOfType:@"png" inDirectory:@"AppIcons"];
+    } else {
+        NSLog(@"Alternate icons are not supported on this device.");
+    }
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
@@ -43,17 +47,17 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
-
+    
     UIImage *appIconImage = [UIImage imageWithContentsOfFile:self.appIcons[indexPath.row]];
     UIImage *resizedIconImage = [self resizedImageWithImage:appIconImage];
     
     UIImageView *imageView = [[UIImageView alloc] initWithImage:resizedIconImage];
     imageView.contentMode = UIViewContentModeScaleAspectFit;
     imageView.frame = cell.contentView.bounds;
-    imageView.layer.cornerRadius = imageView.frame.size.width / 2;
+    imageView.layer.cornerRadius = 10.0;
     imageView.clipsToBounds = YES;
     [cell.contentView addSubview:imageView];
-
+    
     return cell;
 }
 
@@ -62,9 +66,8 @@
     UIImage *selectedIconImage = [UIImage imageWithContentsOfFile:self.selectedIconFile];
     UIImage *resizedSelectedIconImage = [self resizedImageWithImage:selectedIconImage];
     self.iconPreview.image = resizedSelectedIconImage;
-    self.iconPreview.layer.cornerRadius = resizedSelectedIconImage.size.width / 2;
-    self.iconPreview.clipsToBounds = YES;
-
+    
+    // Set the selected icon as the app's icon
     [[UIApplication sharedApplication] setAlternateIconName:[self.selectedIconFile lastPathComponent] completionHandler:^(NSError * _Nullable error){
         if (error) {
             NSLog(@"Error setting alternate icon: %@", error.localizedDescription);
